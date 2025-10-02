037c8bc87c462eba93593d843710fcaa2222069b
denise.bonetti_isaro <denise.bonetti@isarome.it>
2025-10-02 10:44:03 +0200

    10:43

---
+++ src/HomePage.java
@@ -0,0 +1,44 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class HomePage extends JFrame {
+    private JLabel scrittaIniziale = new JLabel("Gestione Orari e Sostituzioni - Scuola IS Saraceno-Romegialli");
+    public JButton sostituzioni = new JButton("Sostituzioni");
+    public JButton orario = new JButton("Orario");
+    public JButton caricaFile = new JButton("Carica File");
+
+    public HomePage() {
+        super("HomePage");
+        getContentPane().setLayout(new BorderLayout(10, 10));
+        JPanel pannelloScritta = new JPanel(new GridLayout(2, 1));
+        pannelloScritta.add(scrittaIniziale);
+        scrittaIniziale.setHorizontalAlignment(SwingConstants.CENTER);
+
+
+
+        JPanel pannelloBottoni = new JPanel(new GridLayout(1, 3, 5, 0));
+        pannelloBottoni.add(sostituzioni);
+        pannelloBottoni.add(orario);
+        pannelloBottoni.add(caricaFile);
+
+        pannelloScritta.add(pannelloBottoni);
+
+        sostituzioni.setBackground(new Color(173, 216, 230));
+        sostituzioni.setForeground(Color.BLACK);
+        orario.setBackground(new Color(173, 216, 230));
+        orario.setForeground(Color.BLACK);
+        caricaFile.setBackground(new Color(173, 216, 230));
+        caricaFile.setForeground(Color.BLACK);
+
+        getContentPane().add(pannelloScritta, BorderLayout.NORTH);
+
+        JPanel container = new JPanel();
+
+        getContentPane().add(container, BorderLayout.CENTER);
+
+        setSize(400, 400);
+        setLocationRelativeTo(null);
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+}
