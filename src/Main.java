a0dafba2fefcf82fa920f35b7075dfbc89937607
alessia <alessia.dellabianca@isarome.it>
2025-11-05 19:48:56 +0100

    19.47

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 import Gestori.GestioneDati;
 import Interfaccia.HomePage;
-
+//gestore
 public class Main {
     public static void main(String[] args) {
         GestioneDati gestore = new GestioneDati();
